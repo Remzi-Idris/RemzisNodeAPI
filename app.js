61e26e6bc572cedba983f7a29c86578f2502cf6b
Remzi-Idris <Remzi.idris@gmail.com>
2023-01-23 10:26:49 +0100

    first init

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const https = require('https')
+
+const instrument = {instuments: [{"name": "Guitarr", "kind": "String instrument", },
+{"name": "Piano", "kind": "PlingPlong instrument" }, 
+{"name": "Violin", "kind": "Stroke instrument"}]}
+
+
+app.get("/joke", (req, res) =>{
+https.get("https://api.chucknorris.io/jokes/random", (response) => {
+    response.on('data', (data) =>{
+        res.send(JSON.parse(data))
+        })
+
+    }).on ("error", (err) => {
+        console.log("There was an error " + err.message)
+    })
+})
+
+app.get("/instruments", (req, res) =>{
+    res.send(instrument)
+})
+
+
+
+app.get("/", (req, res) =>{
+    res.sendFile(__dirname + "/index.html")
+})
+
+// nodemon .\app.js
+// node .\app.js
+
+app.listen(PORT,  () =>{
+    console.log("Listening to port " + PORT)
+})
